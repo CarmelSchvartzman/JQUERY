f368d8d9f822c19a20bba03365e33388d405c026
Carmel Shvartzman <carmel.aspnet@gmail.com>
2019-07-10 09:15:37 +0300

    Create SCROLLTOP.js

---
+++ SCROLLTOP.js
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $(window).on(
+  {'scroll':function(){
+         const w = $(window);
+  	     const b = $('button');
+   		 alert(w.scrollTop());           <<<<<<<<<<<<<<<<<<<<<<<<
+         b.css({'position':'relative','top':w.scrollTop()});       <<<<<<<<<<<<<<
+  	}
+  }
+  );
+});
+</script>
+</head>
+<body>
+
+<div style="border:1px solid black;width:100px;height:150px;overflow:auto">
+This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text. This is some text.</div><br>
+
+<button>Return the vertical position of the scrollbar</button>
+<p>Move the scrollbar down and click the button again.</p>
+<p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p><p>Move the scrollbar down and click the button again.</p>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
